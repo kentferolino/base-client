98d83dfef9d65cb6f37a038b63553244c5d69bef
Kent <kentfero@gmail.com>
2019-06-01 19:00:50 +0800

    Refactor Shopping and Navbar components.

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,29 +1,44 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import PropTypes from "prop-types";
-import ShoppingList from "./ShoppingList";
-import ItemModal from "./ItemModal";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import ShoppingList from './ShoppingList';
+import ItemModal from './ItemModal';
 
-import { connect } from "react-redux";
-import { addItem } from "../actions/itemActions";
-import Button from "@material-ui/core/Button";
+import { addItem } from '../actions/itemActions';
 
 class Shopping extends Component {
   static propTypes = {
-    auth: PropTypes.object.isRequired,
-    addItem: PropTypes.func.isRequired
+    auth: PropTypes.shape({
+      token: PropTypes.string,
+      isAuthenticated: PropTypes.bool,
+      isLoading: PropTypes.bool,
+      user: PropTypes.instanceOf(Object),
+    }),
+    addItemAction: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    auth: {
+      token: null,
+      isAuthenticated: null,
+      isLoading: null,
+      user: null,
+    },
   };
 
   state = {
-    itemModal: false
+    itemModal: false,
   };
 
   toggle = () => {
-    this.setState({ itemModal: !this.state.itemModal });
+    const { itemModal } = this.state;
+    this.setState({ itemModal: !itemModal });
   };
 
   render() {
-    const { auth, addItem } = this.props;
+    const { auth, addItemAction } = this.props;
     const { isAuthenticated } = auth;
     const { itemModal } = this.state;
 
@@ -34,7 +49,7 @@ class Shopping extends Component {
         ) : (
           <h4 className="mb-3 ml-4">Please login to manage items</h4>
         )}
-        <ItemModal visible={itemModal} toggle={this.toggle} addItem={addItem} />
+        <ItemModal visible={itemModal} toggle={this.toggle} addItem={addItemAction} />
         <ShoppingList />
       </div>
     );
@@ -42,10 +57,10 @@ class Shopping extends Component {
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(
   mapStateToProps,
-  { addItem }
+  { addItemAction: addItem },
 )(Shopping);
--- src/components/navs/Navbar.js
+++ src/components/navs/Navbar.js
@@ -1,32 +1,47 @@
-import React, { PureComponent, Fragment } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import AppBar from "@material-ui/core/AppBar";
-import classNames from "classnames";
-import IconButton from "@material-ui/core/IconButton";
-import LoginModal from "../auth/LoginModal";
-import Logout from "../auth/Logout";
-import MenuIcon from "@material-ui/icons/Menu";
-import RegisterModal from "../auth/RegisterModal";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import classNames from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import RegisterModal from '../auth/RegisterModal';
+import Logout from '../auth/Logout';
+import LoginModal from '../auth/LoginModal';
 
 class Navbar extends PureComponent {
   static propTypes = {
     doDrawerOpen: PropTypes.func.isRequired,
     open: PropTypes.bool.isRequired,
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.instanceOf(Object).isRequired,
+    auth: PropTypes.shape({
+      token: PropTypes.string,
+      isAuthenticated: PropTypes.bool,
+      isLoading: PropTypes.bool,
+      user: PropTypes.instanceOf(Object),
+    }),
+  };
+
+  static defaultProps = {
+    auth: {
+      token: null,
+      isAuthenticated: null,
+      isLoading: null,
+      user: null,
+    },
   };
 
   render() {
-    const { classes, open, doDrawerOpen } = this.props;
-    const { isAuthenticated, user } = this.props.auth;
+    const { classes, open, doDrawerOpen, auth } = this.props;
+    const { isAuthenticated = false, user = null } = auth;
 
     const authLinks = (
       <Fragment>
         <span className="navbar-text mr-3">
-          <strong> {user ? `Welcome ${user.name}` : ""} </strong>
+          <strong>{user ? `Welcome ${user.name}` : ''}</strong>
         </span>
         <Logout />
       </Fragment>
@@ -43,7 +58,7 @@ class Navbar extends PureComponent {
       <AppBar
         position="fixed"
         className={classNames(classes.appBar, {
-          [classes.appBarShift]: open
+          [classes.appBarShift]: open,
         })}
       >
         <Toolbar disableGutters={!open}>
@@ -53,18 +68,14 @@ class Navbar extends PureComponent {
               aria-label="Open drawer"
               onClick={doDrawerOpen}
               className={classNames(classes.menuButton, {
-                [classes.hide]: open
+                [classes.hide]: open,
               })}
             >
               <MenuIcon />
             </IconButton>
           )}
           <Typography variant="h6" color="inherit" className={classes.grow}>
-            {isAuthenticated ? (
-              <Link to="/shop">Shopping List</Link>
-            ) : (
-              <Link to="/">Shopping</Link>
-            )}
+            {isAuthenticated ? <Link to="/shop">Shopping List</Link> : <Link to="/">Shopping</Link>}
           </Typography>
           {isAuthenticated ? authLinks : guestLinks}
         </Toolbar>
@@ -74,10 +85,10 @@ class Navbar extends PureComponent {
 }
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(
   mapStateToProps,
-  null
+  null,
 )(Navbar);
