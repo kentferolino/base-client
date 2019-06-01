9982e37f9ea3e2bba530f17abebafd4c9bdc2565
Kent <kentfero@gmail.com>
2019-06-01 17:19:39 +0800

    Refactor Logout component.

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,20 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import { logout } from '../../actions/authActions'
+import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import Proptypes from 'prop-types';
+import { logout } from '../../actions/authActions';
 
-class Logout extends Component {
+class Logout extends PureComponent {
   static propTypes = {
-    logout: Proptypes.func.isRequired
-  }
+    logoutAction: Proptypes.func.isRequired,
+  };
+
   render() {
+    const { logoutAction } = this.props;
     return (
       <Fragment>
-        <Button onClick={this.props.logout} href='#'>Logout</Button>
+        <Button onClick={logoutAction} href="#">
+          Logout
+        </Button>
       </Fragment>
     );
   }
 }
 
-export default connect(null, { logout })(Logout);
+export default connect(
+  null,
+  { logoutAction: logout },
+)(Logout);
